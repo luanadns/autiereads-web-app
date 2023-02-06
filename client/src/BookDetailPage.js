8b007264121fd376cecda554f49452f6c0fe1893
Catalina Singer <catalina.singer@gmail.com>
2023-02-06 18:55:08 +0100

    code working

--- client/src/BookDetailPage.js
+++ client/src/BookDetailPage.js
@@ -1,42 +1,36 @@
-//import {useState, useEffect} from "react"
-// import { useParams } from "react-router-dom"
+import {useState, useEffect} from "react"
+import { useParams } from "react-router-dom"
 import BooksHero from "./components/BooksHero"
 
 export default function BookDetailPage() {
-  //const [books, setBooks] = useState([])
-  //const [book, setBook] = useState(null)
-  // const {id} = useParams()
+  const [books, setBooks] = useState([])
+  const [book, setBook] = useState(null)
+  const {id} = useParams()
 
-  /*useEffect(() => {
+  useEffect(() => {
     const fetchBooks = async () => {
-      const url = 'http://localhost:5050/books'
-      const response = await fetch(url, {
+    const url = 'http://localhost:5050/books'
+    const response = await fetch(url, {
         method: 'GET'
       })
       console.log(response)
       const books = await response.json()
+      const book = books.find(book => book.id === parseInt(id))
+      setBook(book)
       setBooks(books)
     }
 
     fetchBooks()
   }, [])
-*/
-
-  /*useEffect(() => {
-    const fetchBookById = (id) => {
-      return books.find(book => book.id === parseInt(id))
-    };
-    const book = fetchBookById(id)
-    setBook(book)
-  }, [id])
-*/
 
   return (
+    <>
     <div>
       <BooksHero title="AutieReads"/>
     </div>
-    // {book? ? <h2>{book.title} by {book.author}</p> 
-    //<p>{book.description}<p>
-    //: null}}
+    {book ? <div><h2>{book.title} by {book.author}</h2> 
+    <p>{book.description}</p></div>
+    : null}
+    </>
   )
 }
--- client/src/components/BooksListItem.js
+++ client/src/components/BooksListItem.js
@@ -4,7 +4,9 @@ import {Link} from 'react-router-dom';
 export default function BooksListItem({ title, author, id}) {
   return (
     <li>
-      <Link to={`/books/${id}`} ><span className="title">{title}</span> by {author}</Link>
+      <Link to={`/books/${id}`}>
+        <span className="title">{title}</span> by <span className="author">{author}</span>
+      </Link>
     </li>
   )
 }
