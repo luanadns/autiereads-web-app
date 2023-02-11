820b235016f1bd71168e5e37e6ead5f2edf99610
Luana DNS <ludiasnsilva@gmail.com>
2023-02-11 14:55:27 +0000

    Now the images are display with the book description

--- client/src/BookDetailPage.js
+++ client/src/BookDetailPage.js
@@ -1,40 +1,43 @@
-import './BookDetailPage.css';
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import BooksHero from "./components/BooksHero"
+import "./BookDetailPage.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import BooksHero from "./components/BooksHero";
 
 export default function BookDetailPage() {
-  const [books, setBooks] = useState([])
-  const [book, setBook] = useState(null)
-  const {id} = useParams()
+	const [books, setBooks] = useState([]);
+	const [book, setBook] = useState(null);
+	const { id } = useParams();
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-    const url = 'http://localhost:5050/books'
-    const response = await fetch(url, {
-        method: 'GET'
-      })
-      console.log(response)
-      const books = await response.json()
-      const book = books.find(book => book.id === parseInt(id))
-      setBook(book)
-      setBooks(books)
-    }
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const url = "http://localhost:5000/books";
+			const response = await fetch(url, {
+				method: "GET"
+			});
+			console.log(response);
+			const books = await response.json();
+			const book = books.find(book => book.id === parseInt(id));
+			setBook(book);
+			setBooks(books);
+		};
 
-    fetchBooks()
-  }, [])
+		fetchBooks();
+	}, []);
 
-  return (
-    <>
-    <div>
-      <BooksHero title="AutieReads"/>
-    </div>
-    {book ? 
-    <div className="bookDetail-wrapper">
-      <h3>{book.title} by {book.author}</h3>
-      <p>{book.description}</p>
-      </div>
-    : null}
-    </>
-  )
+	return (
+		<>
+			<div>
+				<BooksHero title="AutieReads" />
+			</div>
+			{book ? (
+				<div className="bookDetail-wrapper">
+					<h3>
+						{book.title} by {book.author}
+					</h3>
+					<p>{book.description}</p>
+					<img src={book.image}></img>
+				</div>
+			) : null}
+		</>
+	);
 }
--- client/src/BooksListPage.js
+++ client/src/BooksListPage.js
@@ -1,47 +1,44 @@
-import {useEffect, useState} from 'react'
-import BooksHero from './components/BooksHero'
-import BooksSearchBar from './components/BooksSearchBar'
-import BooksList from './components/BooksList'
-import FilteredBooksList from './components/FilteredBooksList';
+import { useEffect, useState } from "react";
+import BooksHero from "./components/BooksHero";
+import BooksSearchBar from "./components/BooksSearchBar";
+import BooksList from "./components/BooksList";
+import FilteredBooksList from "./components/FilteredBooksList";
 
 const BooksListPage = () => {
-  const [books, setBooks] = useState([])
-  const [query, setQuery] = useState('')
+	const [books, setBooks] = useState([]);
+	const [query, setQuery] = useState("");
 
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const url = "http://localhost:5000/books";
+			const response = await fetch(url, {
+				method: "GET"
+			});
+			console.log(response);
+			const books = await response.json();
+			setBooks(books);
+		};
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const url = 'http://localhost:5050/books'
-      const response = await fetch(url, {
-        method: 'GET'
-      })
-      console.log(response)
-      const books = await response.json()
-      setBooks(books)
-    }
+		fetchBooks();
+	}, []);
 
-    fetchBooks()
-  }, [])
+	const handleChange = event => {
+		const query = event.target.value;
+		setQuery(query);
+	};
 
-  const handleChange = (event) => {
-    const query = event.target.value;
-    setQuery(query);
-  };
+	const filteredBooks = books.filter(book => {
+		return book.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+	});
 
-  const filteredBooks = books.filter((book) => {
-    return book.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-  });
+	return (
+		<div>
+			<BooksHero title="AutieReads">
+				<BooksSearchBar onChange={handleChange} />
+			</BooksHero>
+			{filteredBooks ? <FilteredBooksList filteredBooks={filteredBooks} /> : <BooksList books={books} />}
+		</div>
+	);
+};
 
-  return (
-  <div>
-    <BooksHero title="AutieReads">
-      <BooksSearchBar onChange={handleChange}/>
-    </BooksHero>
-    {filteredBooks? <FilteredBooksList filteredBooks={filteredBooks} />
-    :
-    <BooksList books={books} />}
-  </div>
-  )
-}
-
-export default BooksListPage
+export default BooksListPage;
